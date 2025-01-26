47ccdcd666aeece8d77b3cabe9054bd785a9320f
NIMISHA K <nimisha2721@gmail.com>
2025-01-26 19:12:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+  window.onload = function() {
+    // Show the offer modal when the page loads
+    var offerModal = new bootstrap.Modal(document.getElementById('offerModal'));
+    offerModal.show();
+  };
+
